0725552850cc4e58e2cefe4492219377e352a52a
Aparna Bhaskaran <aparnabhaskaran@Aparnas-MacBook-Air.local>
2022-11-25 15:49:14 -0500

    Implemented how it works section of home page and made it responsive

--- sample/src/pages/home/index.js
+++ sample/src/pages/home/index.js
@@ -1,14 +1,38 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Illustration from '../../assets/Illustration.png';
-import Create from '../../assets/Create.png';
-import Curate from '../../assets/Curate.png';
-import Collaborate from '../../assets/Collaborate.png'
 import Frame from '../../assets/FrameTop.png';
 import SquiggleBlue from '../../assets/SquiggleBlue.png';
 import FrameBlue1 from '../../assets/FrameBlue1.png';
-import FrameBlue2 from '../../assets/FrameBlue2.png';
-import { Typography } from '@material-ui/core';
-import SquiggleBlue2  from '../../assets/SquiggleBlue2.png'
+import { Typography, Grid, Box } from '@material-ui/core';
+import WorkText from '../../assets/WorkText.png';
+import Step1 from '../../assets/Step 1.png';
+import illustration1 from '../../assets/illustration1.png';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import illustration2 from '../../assets/illustration2.png';
+import illustration3 from '../../assets/illustration3.png';
+
+
+const theme = createTheme();
+
+theme.typography.h3 = {
+    fontSize: '1.2rem',
+    '@media (min-width:600px)': {
+      fontSize: '1.8rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '2.6rem',
+    },
+  };
+  
+  theme.typography.h6 = {
+      fontSize: '0.75rem',
+      '@media (min-width:600px)': {
+        fontSize: '1rem',
+      },
+      [theme.breakpoints.up('md')]: {
+        fontSize: '1.4rem',
+      },
+    };
 
 export const Home = () => {
 
@@ -21,49 +45,95 @@ export const Home = () => {
 
     return (
        <div>
-            <div style={{ paddingLeft: '10%', paddingRight: '10%', position: 'relative'}}>
+            <div style={{ paddingLeft: '10%', paddingRight: '10%'}}>
 
-                <div style={{ height: '100%', textAlign: 'center', position: "absolute"}}>
-                    <img src={Illustration} style={{ maxWidth: '100%', height: 'auto' }} />
+                <div style={{position: 'relative'}}>
+                    <div style={{ height: '100%', textAlign: 'center', position: "absolute" }}>
+                        <img src={Illustration} style={{ maxWidth: '100%', height: 'auto' }} />
+                    </div>
+
+                    <div style={{ height: '100%', position: "relative" }} >
+                        <img src={Frame} style={{ paddingTop: '51%', maxWidth: '100%', height: 'auto', zIndex: 1 }} />
+                    </div>
                 </div>
 
-                <div style={{ height: '100%', position: "relative"}} >
-                    <img src={Frame} style={{ paddingTop: '51%', maxWidth: '100%', height: 'auto', zIndex: 1}} />
-                    <img src={SquiggleBlue} onMouseOver={over} onMouseOut={out} style={{ maxWidth: '100%', height: 'auto'}} />
+                <div id="how it works" style={{height: '100%', paddingTop: '10%'}}>
+                    <div id="heading" style={{ position:'absolute'}}>
+                        <img src={WorkText} style={{ maxWidth: '100%', height: 'auto' }}/>
+                    </div>
+                    <div id="create" style={{ position: 'relative', paddingTop: '32%', zIndex: '-1'}}>
+                        <div style={{ height: 'auto', maxWidth: 'auto', borderRadius: '3vw', paddingTop: '4%', paddingBottom: '4%', backgroundColor: '#33FF99', display: 'flex', alignItems: 'center'}}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} style={{ paddingLeft: '5rem' }}>
+                                        <Typography variant="h3" style={{ fontWeight: '800', fontSize: '4.5vw', lineHeight: '5vw' }} sx={{
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
+                                            display: "-webkit-box",
+                                            WebkitLineClamp: "2",
+                                            WebkitBoxOrient: "vertical",
+                                        }}>
+                                            Create a
+                                            Free Account
+                                        </Typography>
+                                        <Typography variant="h6" style={{ fontWeight: '600', fontSize: '2.8vw', lineHeight: '3.5vw', color: '#6600FF', paddingTop: '3%' }}>
+                                            Use your account to browse artworks and discover artists in the CAIR community.
+                                        </Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <img src={illustration1} style={{maxWidth: '100%', height: 'auto'}}/>
+                                </Grid>
+                            </Grid>   
+                        </div>
+                    </div>
+                    
+                    <div id="openWallet" style={{ paddingTop: '2%', zIndex: '-1'}}>
+                        <div style={{ height: 'auto', maxWidth: 'auto', borderRadius: '3vw', paddingTop: '4%', paddingBottom: '4%', backgroundColor: '#33FF99', display: 'flex', alignItems: 'center'}}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} style={{ paddingLeft: '5rem' }}>
+                                        <Typography variant="h3" style={{ fontWeight: '800', fontSize: '4.5vw', lineHeight: '5vw' }} sx={{
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
+                                            display: "-webkit-box",
+                                            WebkitLineClamp: "2",
+                                            WebkitBoxOrient: "vertical",
+                                        }}>
+                                            Open a Wallet
+                                        </Typography>
+                                        <Typography variant="h6" style={{ fontWeight: '600', fontSize: '2.8vw', lineHeight: '3.5vw', color: '#6600FF', paddingTop: '3%' }}>
+                                            Wallets allow artwork on CAIR to contain a historical record of all of its past activity.
+                                        </Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <img src={illustration2} style={{maxWidth: '100%', height: 'auto'}}/>
+                                </Grid>
+                            </Grid>   
+                        </div>
+                    </div>
+
+                    <div id="collectCurate" style={{ paddingTop: '2%', zIndex: '-1'}}>
+                        <div style={{ height: 'auto', maxWidth: 'auto', borderRadius: '3vw', paddingTop: '4%', paddingBottom: '4%', backgroundColor: '#33FF99', display: 'flex', alignItems: 'center'}}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6} style={{ paddingLeft: '5rem' }}>
+                                        <Typography variant="h3" style={{ fontWeight: '800', fontSize: '4.5vw', lineHeight: '5vw' }} sx={{
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
+                                            display: "-webkit-box",
+                                            WebkitLineClamp: "2",
+                                            WebkitBoxOrient: "vertical",
+                                        }}>
+                                            Collect, Curate, and Collaborate
+                                        </Typography>
+                                        <Typography variant="h6" style={{ fontWeight: '600', fontSize: '2.8vw', lineHeight: '3.5vw', color: '#6600FF', paddingTop: '3%' }}>
+                                            Artists in the CAIR community will always have control over how their work is bought, sold, or presented.
+                                        </Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <img src={illustration3} style={{maxWidth: '100%', height: 'auto'}}/>
+                                </Grid>
+                            </Grid>   
+                        </div>
+                    </div>
                 </div>
-
-
-                {/* <div style={{paddingLeft: '70%'}}>
-                        <img src={SquiggleBlue2} />
-                    </div>
-                    <div style={{paddingLeft: '30%'}}>
-                        <div style={{ height: '358px', width: '752px', borderRadius: '60px', backgroundColor: '#3366FF', display: 'flex', alignItems: 'center' }}>
-                            <Typography variant='h3' style={{ color: '#33FF99', fontWeight: '600', fontSize: '66px', lineHeight: '57px' }}>It uses the innovation of blockchain technology to authenticate, store historical records.</Typography>
-                        </div>
-                    </div>   */}
-
-                {/* <div id="how it works" style={{textAlign: 'center'}}>
-                    <Typography varient='h2' style={{fontWeight: '600', fontSize: '90px'}}>How it Works</Typography>
-                    <div style={{height: '485px', width: '1168px', borderRadius: '60px', backgroundColor: '#33FF99', display: 'flex', alignItems: 'center'}}>
-                        <Typography varient='h6' style={{fontWeight: '600', fontSize: '50px'}}>Create a Free Account</Typography><br />
-                        <div>
-                            <Typography varient='h6' style={{fontWeight: '500', fontSize: '35px', color: '#6600FF'}}>Use your account to browse artworks and discover artists in the CAIR community.</Typography>
-                        </div>
-                    </div><br />
-                    <div style={{height: '485px', width: '1168px', borderRadius: '60px', backgroundColor: '#33FF99', display: 'flex', alignItems: 'center'}}>
-                        <Typography varient='h6' style={{fontWeight: '600', fontSize: '50px'}}>Open a Wallet</Typography><br />
-                        <div>
-                            <Typography varient='h6' style={{fontWeight: '500', fontSize: '35px', color: '#6600FF'}}>Wallets allow artwork on CAIR to contain a historical record of all of its past activity.</Typography>
-                        </div>
-                    </div><br />
-                    <div style={{height: '485px', width: '1168px', borderRadius: '60px', backgroundColor: '#33FF99', display: 'flex', alignItems: 'center'}}>
-                        <Typography varient='h6' style={{fontWeight: '600', fontSize: '50px'}}>Collect, Curate, and Collaborate</Typography><br />
-                        <div>
-                            <Typography varient='h6' style={{fontWeight: '500', fontSize: '35px', color: '#6600FF'}}>Artists in the CAIR community will always have control over how their work is bought, sold, or presented.</Typography>
-                        </div>
-                    </div>
-                </div> */}
-
                 
             </div>
         
